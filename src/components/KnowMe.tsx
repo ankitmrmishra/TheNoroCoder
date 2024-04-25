f3d37d009870a2b1535fdccddcc599329c8574cc
Sanskaari <ankitmrmishra1118@gmail.com>
2024-04-26 02:41:55 +0530

    wrg

--- src/components/KnowMe.tsx
+++ src/components/KnowMe.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import backgroundImage from '../../public/bggrad.jpg'
-import Image from 'next/image';
-import { BackgroundGradient } from './ui/phot';
-import MyPhoto from '../app/assets/WhatsApp Image 2024-04-21 at 20.39.22_7cc2dc0b.jpg'
-
+import React from "react";
+import backgroundImage from "../../public/bggrad.jpg";
+import Image from "next/image";
+import { BackgroundGradient } from "./ui/phot";
+import MyPhoto from "../app/assets/WhatsApp Image 2024-04-21 at 20.39.22_7cc2dc0b.jpg";
 
 function KnowMe() {
   return (
     <div className='bg-black flex align-middle flex-col justify-center items-center r'>
       <div className=' text-white inline-block text-transparent text-2xl bg-clip-text lg:text-4xl font-semibold text-center '>
-        The Underdog <span className='text-[#DA0037]'>DEV</span> <br />
+        The Underdog <span className='text-[#DA0037]'>DEV</span>
+        <br />
         <p className=' lg:w-[40rem] w-[20rem] text-sm mt-5 text-white/40'>
           Sure, I may not be a coding guru (yet), but I make up for it with
           grit, determination, and an extraordinarily high tolerance for
@@ -18,29 +18,32 @@ function KnowMe() {
       </div>
       <div className='mt-6 lg:bg-gradient-to-l bg-gradient-to-r from-gray-700 via-gray-900 to-black w-[80%] p-4 rounded-2xl lg:flex lg:p-8'>
         <div className='leftIntro lg:w-[50%]'>
-          <p className='lg:w-[40rem]  mt-5  font-extrabold md:text-4xl text-xl lg:p-4 text-white'>
-            Who Is This "Pro" Anyway?
+          <p className='lg:w-[40rem] mt-5 font-extrabold md:text-4xl text-xl lg:p-4 text-white'>
+            Who Is This 'Pro' Anyway?
           </p>
           <div className='flex flex-col justify-center items-center text-start'>
             <div className='bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 text-transparent bg-clip-text p-5 font-bold text-sm md:text-xl gap-4 flex flex-col'>
               <div>
-                Hi there! I am the so-called "pro" coder behind this questionable
-                website.
+                {" "}
+                Hi there! I am the so-called PRO coder behind this
+                questionable website.{" "}
               </div>
               <div>
+                {" "}
                 In reality, I am just a scrappy self-taught developer with more
-                enthusiasm than skills.
+                enthusiasm than skills.{" "}
               </div>
               <div>
+                {" "}
                 I spend my days wielding code like a chimp with a keyboard,
-                hoping something halfway functional materializes.
+                hoping something halfway functional materializes.{" "}
               </div>
               <div>My expertise?</div>
               <div>Making things work...eventually (maybe).</div>
             </div>
           </div>
         </div>
-        <div className='Image  flex align-middle items-center justify-center md:w-[50%] md:p-8 pl-36 pr-36 pt-3 pb-4'>
+        <div className='Image flex align-middle items-center justify-center md:w-[50%] md:p-8 pl-36 pr-36 pt-3 pb-4'>
           <BackgroundGradient className='w-[18rem]'>
             <Image
               src={MyPhoto}
@@ -56,4 +59,4 @@ function KnowMe() {
   );
 }
 
-export default KnowMe
+export default KnowMe;
